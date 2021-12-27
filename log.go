2684b5f32b566f4cea1c7e1e2e20e2251596f8a1
AgustinSRG <41982624+AgustinSRG@users.noreply.github.com>
2021-12-27 14:35:24 +0100

    Logging utils

---
+++ log.go
@@ -0,0 +1,47 @@
+// Logs
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"sync"
+	"time"
+)
+
+var LOG_MUTEX = sync.Mutex{}
+
+func LogLine(line string) {
+	tm := time.Now()
+	LOG_MUTEX.Lock()
+	defer LOG_MUTEX.Unlock()
+	fmt.Printf("[%s] %s\n", tm.Local(), line)
+}
+
+func LogWarning(line string) {
+	LogLine("[WARNING] " + line)
+}
+
+func LogInfo(line string) {
+	LogLine("[INFO] " + line)
+}
+
+func LogError(err error) {
+	LogLine("[ERROR] " + err.Error())
+}
+
+var LOG_REQUESTS_ENABLED = (os.Getenv("LOG_REQUESTS") != "NO")
+
+func LogRequest(line string) {
+	if LOG_REQUESTS_ENABLED {
+		LogLine("[REQUEST] " + line)
+	}
+}
+
+var LOG_DEBUG_ENABLED = (os.Getenv("LOG_DEBUG") == "YES")
+
+func LogDebug(line string) {
+	if LOG_DEBUG_ENABLED {
+		LogLine("[DEBUG] " + line)
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("RTMP Go Server (Version 1.0.0)")
+	LogInfo("RTMP Go Server (Version 1.0.0)")
 }
